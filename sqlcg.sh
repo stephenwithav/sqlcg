7836bab7978fd27a5f13fced6b778341803cc53b
Steven Edwards <steven@stephenwithav.com>
2023-04-11 17:45:53 -0400

    Export DATABASE_URL so dbmate works.

--- sqlcg.sh
+++ sqlcg.sh
@@ -7,7 +7,7 @@ fi
 
 docker run -e MYSQL_ROOT_PASSWORD=pw --rm --name=db -p 3306:3306 -d mysql:5.7
 DB_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' db)
-DATABASE_URL="mysql://root:pw@${DB_IP}:3306/$1"
+export DATABASE_URL="mysql://root:pw@${DB_IP}:3306/$1"
 dbmate wait
 dbmate up
 sqlc generate
